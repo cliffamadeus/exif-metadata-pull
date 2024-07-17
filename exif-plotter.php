cba6c3d14961aac99adf4c79bcaf4345123b4fb4
Cliffmeister <dev.clfm5t12@gmail.com>
2024-07-17 18:06:29 +0800

    Application changes

--- exif-plotter.php
+++ exif-plotter.php
@@ -21,25 +21,28 @@
 </head>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html" aria-current="page">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="./exif-weather.html" active >Weather</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#welcomeModal">Privacy Policy</a>
-            </li>
-        </ul>
-      </div>
-    </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php" aria-current="page">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./exif-weather.php">Weather</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./exif-plotter.php">Plotter</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#welcomeModal">Privacy Policy</a>
+                  </li>
+                </ul>
+            </div>
+        </div>
 </nav>
 
 <body>
--- index.php
+++ index.php
@@ -20,8 +20,7 @@
     </style>
 </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,10 +29,13 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.html" aria-current="page">Home</a>
+                        <a class="nav-link" href="index.php" aria-current="page">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./exif-weather.html">Weather</a>
+                        <a class="nav-link" href="./exif-weather.php">Weather</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./exif-plotter.php">Plotter</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#welcomeModal">Privacy Policy</a>
@@ -43,6 +45,8 @@
         </div>
     </nav>
 
+<body>
+   
     <div class="container mt-4">
         <!-- Button to trigger modal -->
         <button type="button" class="btn btn-secondary mt-3 text-white" id="modalTriggerBtn" data-bs-toggle="modal" data-bs-target="#welcomeModal">
